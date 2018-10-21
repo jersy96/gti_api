0caae121449246f40563458d31f11cbbdc5c6e6c
Javier Roncallo <jroncallo96@gmail.com>
2018-10-21 17:50:23 -0500

    Se crea el controlador SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  rescue_from Exceptions::InvalidAuthenticationType, with: :if_auth_error
+  rescue_from Exceptions::InvalidCredentials, with: :if_auth_error
+
+  def create
+    auth = Authentication.new(params.require(:credentials), params[:type])
+    auth.create_session
+    render json: auth.token, status: :ok
+  end
+
+  def destroy
+    authenticate_with_http_token do |key, options|
+      token = Token.find_by(secret: key)
+      if token
+        token.destroy
+        render json: token, status: :ok 
+      else
+        render json: {token: I18n.t('errors.messages.token_not_found')},  status: :bad_request
+      end 
+    end 
+  end
+
+  private
+  def if_auth_error(exception)
+    render json: {auth_error: exception.message}, status: :unprocessable_entity
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Resources
   resources :users, only: [:create]
+  resources :sessions, only: [:create, :destroy]
 end
