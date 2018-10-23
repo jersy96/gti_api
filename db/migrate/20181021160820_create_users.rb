093acecf83aca3f856cd9c27c28cbb1b6696f35d
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 11:10:03 -0500

    Se cambia el nombre del campo role por type en el modelo User

--- db/migrate/20181021160820_create_users.rb
+++ db/migrate/20181021160820_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :type
       t.string :email
       t.string :password_digest
-      t.integer :role
       t.string :first_name
       t.string :second_name
       t.string :first_lastname
