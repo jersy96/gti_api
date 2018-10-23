f4ad8330a64a50de95cf854f7aaca7db6a19f9c2
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 16:38:16 -0500

    Se crea el metodo become_tutor en el modelo Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,22 @@ class Student < User
 
   delegate :university, :university=, :career, :career=, to: :lazily_build_profile
 
+  def become_tutor(tutor_params={})
+    self.type = 'Tutor'
+    self.save
+    tutor = Tutor.find(self.id)
+    tutor.attributes = tutor_params
+    if tutor.valid?
+      self.profile.destroy
+      tutor.save
+    else
+      tutor2 = Tutor.find(self.id)
+      tutor2.type = 'Student'
+      tutor2.save
+    end
+    tutor
+  end
+
   private
   def lazily_build_profile
     profile || build_profile
