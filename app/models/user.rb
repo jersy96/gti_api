98220cf0960e9661149ee68bb5197f36204ea09e
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 12:48:28 -0500

    Se eliminan las validaciones del campo role en el modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   enum role: {admin:0, tutor:1, student:2}
 
   #Validations
-  validates :email, :password, :role, :first_name, :first_lastname, presence: true
+  validates :email, :password, :first_name, :first_lastname, presence: true
   validates :email, format: {with: /\A(^\S+)@(.[a-zA-Z0-9]+)+\z/}, allow_blank: true
   validates :email, uniqueness: true, allow_blank: true
   validates :phone, :cellphone, numericality: true, allow_blank: true
