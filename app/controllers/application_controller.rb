c84efb4e2666b521bab5a5314c8d7308791cf75f
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 15:49:58 -0500

    Se crea el metodo set_current_user en ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  rescue_from Exceptions::TokenNotFound, with: :if_token_not_found
+
+  before_action :set_current_user
+
+  private
+  def set_current_user
+    authenticate_with_http_token do |key, options|
+      token = Token.find_by(secret: key)
+      @current_user = token.user if token
+    end
+    raise Exceptions::TokenNotFound unless @current_user
+  end
+
+  def if_token_not_found(exception)
+    render json: {token: exception.message}, status: :unprocessable_entity
+  end
 end
