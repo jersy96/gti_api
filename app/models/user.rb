886b7c5561cb9702c19988f2e666e4fae57f9715
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 13:01:06 -0500

    Se elimina el enum para el campo role en el modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   #Dependencies
   has_secure_password
 
-  #Enums
-  enum role: {admin:0, tutor:1, student:2}
-
   #Validations
   validates :email, :password, :first_name, :first_lastname, presence: true
   validates :email, format: {with: /\A(^\S+)@(.[a-zA-Z0-9]+)+\z/}, allow_blank: true
