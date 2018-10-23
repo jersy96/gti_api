cc4086ad477ca0b261c002f1002f6bf1415fa829
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 14:38:58 -0500

    Se elimina la validacion de presencia del campo password en el modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   #Validations
-  validates :email, :password, :first_name, :first_lastname, presence: true
+  validates :email, :first_name, :first_lastname, presence: true
   validates :email, format: {with: /\A(^\S+)@(.[a-zA-Z0-9]+)+\z/}, allow_blank: true
   validates :email, uniqueness: true, allow_blank: true
   validates :phone, :cellphone, numericality: true, allow_blank: true
