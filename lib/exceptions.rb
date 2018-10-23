5d84294b9a474db7df7ae77e3f0225e3bb965730
Javier Roncallo <jroncallo96@gmail.com>
2018-10-23 15:30:58 -0500

    Se elimina la excepcion UnallowedRole

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,14 +1,4 @@
 module Exceptions
-  class UnallowedRole < StandardError
-    def initialize(role)
-      @role = role 
-    end 
-
-    def message
-      I18n.t('exceptions.unallowed_role', role: @role)
-    end
-  end
-
   class InvalidCredentials < StandardError
     def message
       I18n.t('exceptions.invalid_credentials')
