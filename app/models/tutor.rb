64acd54a845fd86e8e54c661d5cc9c624d3f9615
Javier Roncallo <jroncallo96@gmail.com>
2018-10-24 12:34:51 -0500

    Se agrega validacion de presencia del campo price en el modelo Tutor

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -3,6 +3,8 @@ class Tutor < User
 
   delegate :description, :description=, :price, :price=, :average, :average=, to: :lazily_build_profile
 
+  validates :price, presence: true, if: Proc.new { |tutor| tutor.type == 'Tutor' }
+
   private
   def lazily_build_profile
     profile || build_profile
