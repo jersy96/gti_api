d370de3d4fda1a0a6e8699948d35b52a2f0d1ee5
Javier Roncallo <jroncallo96@gmail.com>
2018-10-25 11:44:29 -0500

    Se agrega el campo subjects al serializer de Tutor

--- app/serializers/tutor_serializer.rb
+++ app/serializers/tutor_serializer.rb
@@ -1,3 +1,7 @@
 class TutorSerializer < ApplicationSerializer
-  attributes :id, :email, :first_name, :second_name, :first_lastname, :second_lastname, :address, :phone, :cellphone, :description, :price, :average
+  attributes :id, :email, :first_name, :second_name, :first_lastname, :second_lastname, :address, :phone, :cellphone, :description, :price, :average, :subjects
+
+  def subjects
+    serialize_each(object.subjects.order(:name), SubjectSerializer)
+  end
 end
