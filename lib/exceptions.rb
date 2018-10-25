056371302d303b15908faeafe2785cf0c1f610a2
Javier Roncallo <jroncallo96@gmail.com>
2018-10-25 15:15:39 -0500

    Se crea la excepcion UnknownFilterType

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -16,4 +16,14 @@ module Exceptions
       I18n.t('exceptions.token_not_found')
     end
   end
+
+  class UnknownFilterType < StandardError
+    def initialize(filter_type)
+      @filter_type = filter_type
+    end
+
+    def message
+      I18n.t('exceptions.unknown_filter_type', filter_type: @filter_type)
+    end
+  end
 end
