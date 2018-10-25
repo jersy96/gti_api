9a6776a5573fff640eac2bb0ca41c04bf7e93cda
Javier Roncallo <jroncallo96@gmail.com>
2018-10-25 17:33:57 -0500

    Se crea el servicio TutorFilter

---
+++ app/services/tutor_filter.rb
@@ -0,0 +1,21 @@
+class TutorFilter < Filter
+  private
+  def name
+    Tutor.where("first_name || ' ' || second_name || ' ' || first_lastname || ' ' || second_lastname LIKE ?", likefy(@fc['value']))
+  end
+
+  def price
+    prices = [@fc['price1'], @fc['price2']]
+    p1 = prices.min
+    p2 = prices.max
+    Tutor.joins('JOIN tutor_profiles ON tutor_profiles.tutor_id = users.id').where('tutor_profiles.price BETWEEN ? AND ?', p1, p2)
+  end
+
+  def subject
+    Tutor.joins(:subjects).where('subjects.name LIKE ?', likefy(@fc['value']))
+  end
+
+  def likefy(s)
+    "%#{s.split('').join('%')}%"
+  end
+end
