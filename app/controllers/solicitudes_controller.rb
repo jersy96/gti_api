8da909ceefc33c945762552d8767ea20269e98f5
Javier Roncallo <jroncallo96@gmail.com>
2018-10-26 10:47:58 -0500

    Se crea el controlador de Solicitude

---
+++ app/controllers/solicitudes_controller.rb
@@ -0,0 +1,52 @@
+class SolicitudesController < ApplicationController
+  before_action :set_solicitude, only: [:update]
+
+  def create
+    solicitude = Solicitude.new(solicitude_params)
+    if solicitude.save
+      render json: solicitude, status: :ok
+    else
+      render json: solicitude.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @solicitude.attributes = update_params
+    if solicitude.save
+      render json: solicitude, status: :ok
+    else
+      render json: solicitude.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_solicitude
+    @solicitude = Solicitude.find(params[:id])
+  end
+
+  def solicitude_params
+    params.permit(
+      :student_id,
+      :tutor_id,
+      :solicitude_type,
+      :payment_method,
+      :state,
+      :hours,
+      :address,
+      :date,
+      :attendants
+    )
+  end
+
+  def update_params
+    params.permit(
+      :solicitude_type,
+      :payment_method,
+      :state,
+      :hours,
+      :address,
+      :date,
+      :attendants
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :students, only: [:create, :update]
   resources :tutors, only: [:show, :create, :update]
   resources :subjects, only: [:index, :create]
+  resources :solicitudes, only: [:create, :update]
 
   # Namespaces
   namespace :tutors do
