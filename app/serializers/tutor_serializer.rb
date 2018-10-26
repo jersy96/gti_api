999f25b48d4d629bf86e796073cf6c9840eeefad
Javier Roncallo <jroncallo96@gmail.com>
2018-10-26 13:49:23 -0500

    Se agrega el campo comments en el serializer de Tutor

--- app/serializers/tutor_serializer.rb
+++ app/serializers/tutor_serializer.rb
@@ -1,7 +1,11 @@
 class TutorSerializer < ApplicationSerializer
-  attributes :id, :email, :first_name, :second_name, :first_lastname, :second_lastname, :address, :phone, :cellphone, :description, :price, :average, :subjects
+  attributes :id, :email, :first_name, :second_name, :first_lastname, :second_lastname, :address, :phone, :cellphone, :description, :price, :average, :subjects, :comments
 
   def subjects
     serialize_each(object.subjects.order(:name), SubjectSerializer)
+  end
+
+  def comments
+    object.ratings.order('created_at DESC').pluck(:comment).compact
   end
 end
