fa2e85d8f27b53380b141575b36f97c9f61f3401
Javier Roncallo <jroncallo96@gmail.com>
2018-10-28 12:13:19 -0500

    Se elimina validacion de numericalidad en los campos phone y cellphone del modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   validates :email, :first_name, :first_lastname, presence: true
   validates :email, format: {with: /\A(^\S+)@(.[a-zA-Z0-9]+)+\z/}, allow_blank: true
   validates :email, uniqueness: true, allow_blank: true
-  validates :phone, :cellphone, numericality: true, allow_blank: true
 end
