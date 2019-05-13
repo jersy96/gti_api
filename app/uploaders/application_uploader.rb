aa771c538c49af412487333b3e4da3e71e816c3a
Javier Roncallo <jroncallo96@gmail.com>
2019-05-13 15:02:13 -0500

    Se crea el uploader ApplicationUploader

---
+++ app/uploaders/application_uploader.rb
@@ -0,0 +1,19 @@
+class ApplicationUploader < ActiveRecord::Base
+  after :remove, :clear_uploader
+
+  protected
+  def random_filename(model, camp)
+    file_name = ''
+    begin
+      file_name = "#{SecureRandom.uuid.gsub(/-/, '')}#{Pathname.new(original_filename).extname}"
+    end while (model.where(camp => file_name).any?)
+    file_name
+  end
+
+  def clear_uploader
+    if model.persisted?
+      @file = @filename = @original_filename = @cache_id = @version = @storage = nil
+      model.update_column(mounted_as, nil)
+    end
+  end
+end
