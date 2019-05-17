b9b9c95f1139190875ce004dbf1dd268a206fd17
Javier Roncallo <jroncallo96@gmail.com>
2019-05-16 22:14:48 -0500

    Se agrega el campo user al serializer del Story

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,7 +1,11 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :file_type, :file
+  attributes :id, :file_type, :file, :user
 
   def file
     object.complete_file_url
+  end
+
+  def user
+    UserSerializer.new(object.user).as_json[:user]
   end
 end
