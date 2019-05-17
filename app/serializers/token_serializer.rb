83e34648d15f521021ce8327e47c18286e9f049b
Javier Roncallo <jroncallo96@gmail.com>
2019-05-17 07:01:51 -0500

    Se edita el metodo user del serializer de Token

--- app/serializers/token_serializer.rb
+++ app/serializers/token_serializer.rb
@@ -2,6 +2,11 @@ class TokenSerializer < ApplicationSerializer
   attributes :secret, :user
 
   def user
-    UserSerializer.new(object.user).as_json[:user]
+    u = object.user
+    if u.instance_of?(Tutor)
+      TutorSerializer.new(u).as_json[:tutor].merge({type: :tutor})
+    else
+      StudentSerializer.new(u).as_json[:student].merge({type: :student})
+    end
   end
 end
